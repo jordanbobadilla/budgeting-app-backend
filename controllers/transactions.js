8b06e3b3e0f10c90f4974f34c9fc03c632b28e12
Jordan Bobadilla <jordanboba08@gmail.com>
2021-06-15 21:36:55 -0400

    show route done

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,6 +1,10 @@
 const transactions = require("express").Router()
 const transactionsArray = require("../models/transaction")
 
+transactions.get("/:id", (req, res) => {
+    const {id} = req.params
+    transactionsArray[id] ? res.json(transactionsArray[id]) : res.redirect("/404")
+})
 transactions.get("/", (req, res) => {
     res.json(transactionsArray)
 })
