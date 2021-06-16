56ec47fdb87113f26ee76a5b983d522c0ecfdf15
Jordan Bobadilla <jordanboba08@gmail.com>
2021-06-15 21:32:36 -0400

    index route done

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require('cors');
+const transactions = require("./controllers/transactions");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+app.use("/transactions", transactions)
+
 // ROOT
 app.get("/", (req, res) => {
   res.send("Basic Express App - ROOT");
 });
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found")
+})
 
 module.exports = app;
