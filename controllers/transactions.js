5ed9d0ae2b7ed287edb9a904b671eba726be24f6
Jordan Bobadilla <jordanboba08@gmail.com>
2021-06-15 22:25:24 -0400

    destroy route done

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,34 +1,46 @@
-const transactions = require("express").Router()
-const checkTypes = require("../helpers/checkTypes")
-const transactionsArray = require("../models/transaction")
+const transactions = require("express").Router();
+const checkTypes = require("../helpers/checkTypes");
+const transactionsArray = require("../models/transaction");
 
 transactions.post("/", (req, res) => {
-    const correctType = checkTypes(req.body, res)
-    if (correctType) {
-        transactionsArray.push(req.body)
-        res.json(transactionsArray[transactionsArray.length - 1])
-    }
-})
+  const correctType = checkTypes(req.body, res);
+  if (correctType) {
+    transactionsArray.push(req.body);
+    res.json(transactionsArray[transactionsArray.length - 1]);
+  }
+});
 
 transactions.put("/:id", (req, res) => {
-    const {id} = req.params
-    if (transactionsArray[id]) {
-        const correctType = checkTypes(req.body, res)
-        if (correctType) {
-            transactionsArray[id] = req.body
-            res.json(transactionsArray[id])
-        }
-    } else {
-        res.redirect("/404")
+  const { id } = req.params;
+  if (transactionsArray[id]) {
+    const correctType = checkTypes(req.body, res);
+    if (correctType) {
+      transactionsArray[id] = req.body;
+      res.json(transactionsArray[id]);
     }
-})
+  } else {
+    res.redirect("/404");
+  }
+});
+
+transactions.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  if (transactionsArray[id]) {
+    const deletedTransaction = transactionsArray.splice(id, 1)
+    res.json(deletedTransaction);
+  } else {
+    res.redirect("/404");
+  }
+});
 
 transactions.get("/:id", (req, res) => {
-    const {id} = req.params
-    transactionsArray[id] ? res.json(transactionsArray[id]) : res.redirect("/404")
-})
+  const { id } = req.params;
+  transactionsArray[id]
+    ? res.json(transactionsArray[id])
+    : res.redirect("/404");
+});
 transactions.get("/", (req, res) => {
-    res.json(transactionsArray)
-})
+  res.json(transactionsArray);
+});
 
-module.exports = transactions
+module.exports = transactions;
